3f78aba39eb4c97b38cb22b0dead7035d758f6a4
Arun <arungarg57460@gmail.com>
2025-01-15 20:35:30 +0530

    daily

---
+++ Everything.java
@@ -0,0 +1,108 @@
+import java.util.*;
+public class Everything {
+    public static void main ( String args[]){
+        // System.out.print("Hello world");
+        // Scanner obj=new Scanner(System.in);
+        // int n=obj.nextInt();
+        // System.out.println(n+3);
+        // int a=10;
+        // System.out.println(a);
+        // String a ="tony";
+        // // System.out.println(a);
+        // byte a =127;
+        // // char a = 'a';
+        // System.out.println(a);
+        // int a=10;
+        // int b=20;
+        // int sum=a+b;
+        // System.out.println(sum);
+//         Scanner obj = new Scanner(System.in);
+//         int a= obj.nextInt();
+//         // if(a<0){
+//         //     System.out.println(a+" is negative");
+//         // }
+//         // int a = obj.nextInt();
+//         String s;
+// //Problem to ask //////////////////////////////////////////////////////////
+//         switch(a){
+//             case 1:
+//             s="Monday";
+//             break;
+//             case 2:
+//             s="Tuesday";
+//             break;
+//             case 3:
+//             s="wednesday";
+//             break;
+//             case 4:
+//             s="Thursday";
+//             break;
+//             case 5:
+//             s="Friday";
+//             break;
+//             case 6:
+//             s="Saturday";
+//             break;
+//             case 7:
+//             s="Sunday";
+//             break;
+//             default:
+//             s="";
+//             break;
+
+            
+//         }
+//         System.out.println(s);
+        // if((a%4==0&&!(a%100==0))||a%400==0){
+        //     System.out.println("Leap year!!!!");
+        // }
+        // for(int i=0; i<5; i++) {
+        //     System.out.println(i);
+        //     i+=2;
+
+        //     }
+        int a;
+        Scanner obj=new Scanner(System.in);
+        a=obj.nextInt();
+        // int sol=0;
+        // int sol1=0;
+        // // for(int i=a;i>0;i--){
+        // //     sol*=i;
+        // // }
+        // for(int i=0;i<=a;i++){
+        //     if(i%2==0){
+        //         sol+=i;
+        //     }
+        //     else{
+        //         sol1+=i;
+        //     }
+        // }
+
+        // System.out.println(sol+" Even");
+        // System.out..println(sol1+" Odd");
+    //     for(int i=4;i>0;i--){
+    //         for(int j=i;j>0;j--){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println("");
+    //     }
+    // }
+    // int count=2;
+    // for(int i=a;i>0;i--){
+    //     for(int j=i;j>0;j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=count-1;k>0;k--){
+    //         System.out.print(k);
+           
+    //     }
+    //     if(i<a){
+    //     for(int z=2;z<count;z++){
+    //         System.out.print(z);
+    //     }}
+    //     System.out.println("");
+    //     count++;
+    // }
+    
+}
+}

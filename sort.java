a14d316bec4f120154adc6518063225a54c3c947
Arun <arungarg57460@gmail.com>
2025-02-05 20:49:25 +0530

    Daily

--- sort.java
+++ sort.java
@@ -1,5 +1,30 @@
 public class sort {
-    public static void main(String args[]){
+    public static void selection(int[] arr){
+        int i=0;
         
+        while(i<arr.length-1){
+            int j=i;
+            for(int k=i;k<arr.length-1;k++){
+                j=(arr[k]<arr[j]?k:j);
+            }
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+        }
+    }
+    public static void insertion(int[] arr){
+        int e=0;
+        for(int i=0;i<e)
+    }
+    public static void show(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        int[] arr={2,1,3,4,5};
+        selection(arr);
+        show(arr);
     }
 }

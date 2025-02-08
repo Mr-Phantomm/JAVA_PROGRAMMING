b94499208777634ab6724b240218ded967cf0f2e
Arun <arungarg57460@gmail.com>
2025-02-08 14:35:09 +0530

    Dailty

---
+++ L1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class L1 {
+    public static void main(String args[]){
+        int n;
+        Scanner obj=new Scanner(System.in);
+        int s=0;
+        n=obj.nextInt();
+        int[] nums= new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=obj.nextInt();
+        }
+        int e=nums.length-1;
+        while(s<e){
+            int m=s+((s-e)/2);
+            
+        }
+    }
+}

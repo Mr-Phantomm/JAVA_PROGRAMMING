c421a895fd181501c4b510488592f97a202ff0da
Arun <arungarg57460@gmail.com>
2025-02-15 21:25:17 +0530

    Daily

--- problem.java
+++ problem.java
@@ -54,6 +54,26 @@ public class problem {
         endcol--;
     }
     }
+    public static void diagonal(int[][] arr){
+        int n=arr.length;
+        int m=arr[0].length;
+        if(n!=m){
+            return;
+        }
+        int primary=0;
+        int secondary=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    primary+=arr[i][j];
+                }
+                if(i+j==n-1){
+                    secondary+=arr[i][j];
+                }
+            }
+        }
+        System.out.println(primary+" "+secondary);
+    }
     public static void main(String args[]){
         int[][] arr=new int[4][4];
         Scanner obj=new Scanner(System.in);
@@ -62,6 +82,6 @@ public class problem {
                 arr[i][j]=obj.nextInt();
             }
         }
-        spiral(arr);
+        diagonal(arr);
     }
 }

f5cc85af09ac6915a697717a2f8049cfb9b0f73f
Arun <arungarg57460@gmail.com>
2025-02-21 14:47:21 +0530

    daily

--- seven.java
+++ seven.java
@@ -10,6 +10,26 @@ public class seven {
             System.out.println("already 1");
         }
     }
+    static int ans;
+    public static int floorSqrt(int n) {
+        int s=1;
+        int e=n;
+        while(s<e){
+            int mid=s+e/2;
+            if(mid*mid==n){
+                return mid;
+            }
+            if(mid*mid<n){
+                ans=mid;
+                s=mid+1;
+                
+            }
+            if(mid*mid>n){
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
     public static void main(String args[]){
         int n;
         Scanner obj = new Scanner(System.in);
@@ -23,8 +43,10 @@ public class seven {
         // else{
         //     System.out.println("even");
         // }
-        int k=obj.nextInt();
+        // int k=obj.nextInt();
         
-        ithbit(n,k);
+        // ithbit(n,k);
+        System.out.println(floorSqrt(n));
+        
     }
 }

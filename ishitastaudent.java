d16d7daeda253a156605b7082bcd136d1abc6eba
Arun <arungarg57460@gmail.com>
2025-03-03 18:57:20 +0530

    Daaily

--- ishitastaudent.java
+++ ishitastaudent.java
@@ -1,15 +1,28 @@
+import java.util.*;
 public class ishitastaudent {
-    public static void main(String args[]){
-        int[] arr={1,2,-3,-1,-2,-3};
-        int curr=0;
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(curr<0){
-                curr=0;
-            }
-            curr+=arr[i];
-            max=(max>curr?max:curr);
+    public static void count(int n){
+        if(n==1){
+            return;
         }
-        System.out.println(max);
+        
+
+        count(n-1);
+        System.out.println(n);
+
+    }
+    public static void main(String args[]){
+        // int[] arr={1,2,-3,-1,-2,-3};
+        // int curr=0;
+        // int max=Integer.MIN_VALUE;
+        // for(int i=0;i<arr.length;i++){
+        //     if(curr<0){
+        //         curr=0;
+        //     }
+        //     curr+=arr[i];
+        //     max=(max>curr?max:curr);
+        // }
+        // System.out.println(max);
+        int n=10;
+        count(n);
     }
 }

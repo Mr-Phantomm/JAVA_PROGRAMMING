fddb01189c75c0f1bcdba2ef0bbb97eaeb7462da
Arun <arungarg57460@gmail.com>
2025-03-12 22:05:08 +0530

    Daily

--- ishitastudent2.java
+++ ishitastudent2.java
@@ -30,7 +30,29 @@ public static void sequence(String str,String output,int i){
         }
         return shortest(str,i+1,x,y);
     }
+    public static String compress(String str,StringBuilder x,int i,int count){
+        if(i>=str.length()){
+            x.append(str.charAt(i-1));
+            x.append(count);
+            return x.toString();
+        }
+        else if(str.charAt(i)!=str.charAt(i-1)){
+            x.append(str.charAt(i-1));
+            x.append(count);
+            count=1;
+            return compress(str, x, i+1, count);
+        }
+        
+        else{
+            return compress(str, x, i+1, count+1);
+        }
+
+
+    }
     public static void main(String args[]){
+        String str=new String("aaabbbbcccc");
+        StringBuilder str2=new StringBuilder("");
+        System.out.println(compress(str,str2,1,1));
         // String str="abc";
         // String output="";
         // sequence(str,output,0);
@@ -46,23 +68,25 @@ public static void sequence(String str,String output,int i){
                 
         //     }
         // System.out.println(str[max]);
-        String str=" i am arun";
-        StringBuilder str2=new StringBuilder("");
-        int e=(str.charAt(0)==' '?0:1);
-        if(e==1){
-            str2.append(Character.toUpperCase(str.charAt(0)));
+        // String str=" i am arun";
+        // StringBuilder str2=new StringBuilder("");
+        // int e=(str.charAt(0)==' '?0:1);
+        // if(e==1){
+        //     str2.append(Character.toUpperCase(str.charAt(0)));
 
-        }
-        for(int i=e;i<str.length();i++){
-            if(str.charAt(i)== ' '){
-                str2.append(str.charAt(i));
-                i++;
-                str2.append(Character.toUpperCase(str.charAt(i)));
-                continue;
-            }
-            str2.append(str.charAt(i));
+        // }
+        // for(int i=e;i<str.length();i++){
+        //     if(str.charAt(i)== ' '){
+        //         str2.append(str.charAt(i));
+        //         i++;
+        //         str2.append(Character.toUpperCase(str.charAt(i)));
+        //         continue;
+        //     }
+        //     str2.append(str.charAt(i));
 
-        }
-        System.out.println(str2);
+        // }
+        // System.out.println(str2);
+
+
     }
 }
